8f42cd0421cda53f4157a350dabad73b6ccde3f5
Jascha Haldemann <jh@oxon.ch>
2024-02-20 15:38:46 +0100

    add buttonboard uplink event

--- app/controllers/uplinks_controller.rb
+++ app/controllers/uplinks_controller.rb
@@ -19,7 +19,7 @@ class UplinksController < ApplicationController
       # when "20"  # any button clicked
       # when "21"  # any button clicked with image ID
       # when "22"  # specific button clicked
-      when "30"  # Oxobutton event (https://oxobutton.ch/products/oxobutton-lorawan/documentation#uplink))
+      when "30"  # Oxobutton event
         button_number = payload[2..3].to_i(16) unless payload[2..3].nil?
         is_heartbeat = payload[4..5].to_i(16) != 0 unless payload[4..5].nil?
         image_code = payload[8..11]
@@ -30,6 +30,24 @@ class UplinksController < ApplicationController
           event = Order.create!(
             title: device.name,
             text: "{\"btnNr\":#{button_number},\"imgCode\":\"#{image_code}\",\"bat\":#{battery},\"temp\":#{temperature}}",
+            data: params.to_json.to_s,
+            device_id: device.id,
+            user_id: device.user_id,
+          )
+        end
+      when "31"  # Buttonboard event
+        button_number = payload[2..3].to_i(16) unless payload[2..3].nil?
+        is_heartbeat = payload[4..5].to_i(16) != 0 unless payload[4..5].nil?
+        # accel_irq = payload[6..7].to_i(16) != 0 unless payload[6..7].nil?
+        app_mode = payload[8..9].to_i(16) != 0 unless payload[8..9].nil?
+        # enabled_buttons = payload[10..11].to_i(16) != 0 unless payload[10..11].nil?
+        battery = payload[12..13].to_i(16) unless payload[12..13].nil?
+        temperature = payload[14..15].to_i(16) unless payload[14..15].nil?
+        device.update(battery: battery, last_time_seen: Time.now)
+        unless is_heartbeat
+          event = Order.create!(
+            title: device.name,
+            text: "{\"btnNr\":#{button_number},\"appMode\":\"#{app_mode}\",\"bat\":#{battery},\"temp\":#{temperature}}",
             data: params.to_json.to_s,
             device_id: device.id,
             user_id: device.user_id,
